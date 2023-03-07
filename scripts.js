0419e017f81315b16f55107e8ca659df0982d7ed
unknown <C:\Users\xxxx\AppData\Roaming\The Bat!>
2023-03-07 02:10:19 +0100

    Add mobile navigation. Change skills animations behaviour - now they fire only once.

--- scripts.js
+++ scripts.js
@@ -1,19 +1,32 @@
 // Enables skill rows animations after reaching the desired height
-function reveal() {
-  let reveals = document.querySelectorAll(".reveal");
-  for (let i = 0; i < reveals.length; i++) {
-    let windowHeight = window.innerHeight;
-    let elementTop = reveals[i].getBoundingClientRect().top;
-    let elementVisible = 150;
-    if (elementTop < windowHeight - elementVisible) {
-      reveals[i].classList.add("active");
-    } else {
-      reveals[i].classList.remove("active");
+function revealSkills() {
+  const reveals = document.querySelectorAll(".reveal");
+  const windowHeight = window.innerHeight;
+  const elementTop = reveals[0].getBoundingClientRect().top;
+  const sectionVisible = 120;
+
+  if (elementTop < windowHeight - sectionVisible) {
+    for (let i = 0; i < reveals.length; i++) {
+      const timeoutLegth = i * 500;
+      setTimeout(() => reveals[i].classList.add("active"), timeoutLegth);
     }
+    window.removeEventListener("scroll", revealSkills);
   }
 }
-window.addEventListener("scroll", reveal);
-reveal();
+
+window.addEventListener("scroll", revealSkills);
+
+// When on mobile toggles the mobile nav el after 100vh
+const toggleMobileNav = () => {
+  const mobileNav = document.getElementsByClassName("mobile-nav")[0];
+  if (window.scrollY > window.innerHeight) {
+    mobileNav.classList.remove("hidden");
+  } else {
+    mobileNav.classList.add("hidden");
+  }
+};
+
+window.addEventListener("scroll", toggleMobileNav);
 
 // Submits the contact form
 const handleSubmit = (event) => {
@@ -47,3 +60,23 @@ window.onclick = function (event) {
     modal.style.display = "none";
   }
 };
+
+// MOBILE NAV
+
+// Toggle between open/closed states of the mobile menu and change hamburger icon accordingly
+
+const hamburger = document.getElementsByClassName("hamburger")[0];
+const mobileNav = document.getElementsByClassName("mobile-nav__links")[0];
+const [...mobileNavLinks] = document.getElementsByClassName("mobile-nav__link");
+
+hamburger.onclick = function () {
+  hamburger.classList.toggle("hamburger__change-icon");
+  mobileNav.classList.toggle("hidden");
+};
+
+mobileNavLinks.forEach((link) => {
+  link.onclick = () => {
+    hamburger.classList.toggle("hamburger__change-icon");
+    mobileNav.classList.toggle("hidden");
+  };
+});
